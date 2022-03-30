81ccfad8bc2ac625bf5a88bd092826152f7214e7
Mohammad <mohammadpoorjamal@gmail.com>
2022-03-30 18:09:49 +0430

    2- simple form (html only)

---
+++ src/components/YoutubeForm.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const YoutubeForm = () => {
+  return (
+    <div>
+      <form>
+
+        <label htmlFor="name"> Name </label>
+        <input type="text" id="name" name="name" />
+
+        <label htmlFor="email"> E-mail </label>
+        <input type="email" id="email" name="email" />
+
+        <label htmlFor="chanel"> Chanel </label>
+        <input type="text" id="chanel" name="chanel" />
+
+        <button>submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default YoutubeForm;
+
