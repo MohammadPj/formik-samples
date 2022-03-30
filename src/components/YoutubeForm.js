331e135acfcbc54204a09f6c4ff760b97861f4e3
Mohammad <mohammadpoorjamal@gmail.com>
2022-03-30 22:38:16 +0430

    8- form validation

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -1,36 +1,104 @@
-import React from 'react';
-import { useFormik } from 'formik'
+import React from "react";
+import { useFormik } from "formik";
+
+const initialValues = {
+  name: "",
+  email: "",
+  channel: "",
+};
+
+const onSubmit = (values) => {
+  console.log("form data", values);
+};
+
+const validate = (values) => {
+  //  values.name values.email values.channel
+  //  errors.name errors.email errors.channel
+  //  should have same values and errors keys
+  //  errors.name = 'this field is required'
+  const errors = {};
+
+  console.log("values", values);
+  if (!values.name) {
+    errors.name = "Required";
+  }
+
+  if (!values.email) {
+    errors.email = "Required";
+  } else if (
+    !/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/i.test(
+      values.email
+    )
+  ) {
+    errors.email = "Invalid email format";
+  }
+
+  if (!values.channel) {
+    errors.channel = "Required";
+  }
+
+  return errors;
+};
 
 const YoutubeForm = () => {
-
   const formik = useFormik({
-    initialValues: {
-      name: "",
-      email: "",
-      channel: ""
-    },
-    onSubmit: values => {
-      console.log("form data", values)
-    }
-  })
+    initialValues,
+    onSubmit,
+    validate,
+  });
+
+  console.log("errors", formik.errors);
+
+  const handleCancelForm = () => {
+    console.log("formik", formik);
+  };
 
   return (
     <div>
       <form onSubmit={formik.handleSubmit}>
-        <label htmlFor="name"> Name </label>
-        <input type="text" id="name" name="name" onChange={formik.handleChange} value={formik.values.name} />
+        <div className="form-control">
+          <label htmlFor="name"> Name </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            onChange={formik.handleChange}
+            value={formik.values.name}
+          />
+          {formik.errors.name && <div className={"error"}>{formik.errors.name}</div>}
+        </div>
 
-        <label htmlFor="email"> E-mail </label>
-        <input type="email" id="email" name="email" onChange={formik.handleChange} value={formik.values.email} />
+        <div className="form-control">
+          <label htmlFor="email"> E-mail </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            onChange={formik.handleChange}
+            value={formik.values.email}
+          />
+          {formik.errors.email && <div className={"error"}>{formik.errors.email}</div>}
+        </div>
 
-        <label htmlFor="channel"> channel </label>
-        <input type="text" id="channel" name="channel" onChange={formik.handleChange} value={formik.values.channel} />
+        <div className="form-control">
+          <label htmlFor="channel"> channel </label>
+          <input
+            type="text"
+            id="channel"
+            name="channel"
+            onChange={formik.handleChange}
+            value={formik.values.channel}
+          />
+          {formik.errors.channel && <div className={"error"}>{formik.errors.channel}</div>}
+        </div>
 
         <button type={"submit"}>submit</button>
+        <button type={"button"} onClick={handleCancelForm}>
+          cancel
+        </button>
       </form>
     </div>
   );
 };
 
 export default YoutubeForm;
-
